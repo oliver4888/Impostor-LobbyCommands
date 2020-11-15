2c536e2b372885202c8c8e77abc89a11530efa3e
Oliver <8377772+oliver4888@users.noreply.github.com>
2020-11-15 19:40:51 +0000

    Add LobbyCommands project.

---
+++ LobbyCommands/GameEventListener.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Impostor.Api.Events;
+using Impostor.Api.Innersloth;
+using Impostor.Api.Events.Player;
+
+using Microsoft.Extensions.Logging;
+
+namespace LobbyCommands
+{
+    public class GameEventListener : IEventListener
+    {
+        readonly ILogger<LobbyCommandsPlugin> _logger;
+        readonly string[] _mapNames = Enum.GetNames(typeof(MapTypes));
+
+        public GameEventListener(ILogger<LobbyCommandsPlugin> logger) => _logger = logger;
+
+        [EventListener]
+        public async ValueTask OnPlayerChat(IPlayerChatEvent e)
+        {
+            if (e.Game.GameState != GameStates.NotStarted || !e.Message.StartsWith("/") || !e.ClientPlayer.IsHost)
+                return;
+
+            _logger.LogInformation($"Attempting to evaluate command from {e.PlayerControl.PlayerInfo.PlayerName} on {e.Game.Code.Code}. Message was: {e.Message}");
+
+            string[] parts = e.Message[1..].Split(" ");
+
+            switch (parts[0])
+            {
+                case "impostors":
+                    if (parts.Length == 1)
+                    {
+                        await e.PlayerControl.SendChatAsync($"Please specify the number of impostors.");
+                        return;
+                    }
+
+                    if (int.TryParse(parts[1], out int num))
+                    {
+                        num = Math.Clamp(num, 1, 3);
+
+                        await e.PlayerControl.SendChatAsync($"Setting the number of impostors to {num}");
+
+                        e.Game.Options.NumImpostors = num;
+                        await e.Game.SyncSettingsAsync();
+                    }
+                    else
+                        await e.PlayerControl.SendChatAsync($"Unable to convert '{parts[1]}' to a number!");
+                    break;
+                case "map":
+                    if (parts.Length == 1)
+                    {
+                        await e.PlayerControl.SendChatAsync($"Please specify the map. Accepted values: {string.Join(", ", _mapNames)}");
+                        return;
+                    }
+
+                    if (!_mapNames.Any(name => name.ToUpperInvariant() == parts[1].ToUpperInvariant()))
+                    {
+                        await e.PlayerControl.SendChatAsync($"Unknown map. Accepted values: {string.Join(", ", _mapNames)}");
+                        return;
+                    }
+                    
+                    MapTypes map = Enum.Parse<MapTypes>(parts[1], true);
+
+                    await e.PlayerControl.SendChatAsync($"Setting map to {map}");
+
+                    e.Game.Options.Map = map;
+                    await e.Game.SyncSettingsAsync();
+                    break;
+                default:
+                    _logger.LogInformation($"Unknown command {parts[0]} from {e.PlayerControl.PlayerInfo.PlayerName} on {e.Game.Code.Code}.");
+                    break;
+            }
+        }
+    }
+}
---
+++ LobbyCommands/LobbyCommandsPlugin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Impostor.Api.Plugins;
+using Impostor.Api.Events.Managers;
+
+using Microsoft.Extensions.Logging;
+
+namespace LobbyCommands
+{
+    [ImpostorPlugin(
+        package: "uk.ol48.lobbycommands",
+        name: "Lobby Commands",
+        author: "oliver4888",
+        version: "1.0.0")]
+    public class LobbyCommandsPlugin : PluginBase
+    {
+        readonly ILogger<LobbyCommandsPlugin> _logger;
+        readonly IEventManager _eventManager;
+        IDisposable _unregister;
+
+        public LobbyCommandsPlugin(ILogger<LobbyCommandsPlugin> logger, IEventManager eventManager)
+        {
+            _logger = logger;
+            _eventManager = eventManager;
+        }
+
+        public override ValueTask EnableAsync()
+        {
+            _unregister = _eventManager.RegisterListener(new GameEventListener(_logger));
+            return default;
+        }
+
+        public override ValueTask DisableAsync()
+        {
+            _unregister.Dispose();
+            return default;
+        }
+    }
+}
